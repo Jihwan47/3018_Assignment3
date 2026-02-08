e2c003c9a1f53994af440349688707403656c709
JayHwang <hjik7297@gmail.com>
2026-02-08 08:02:19 -0600

    Inital commit

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
+    setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
     testMatch: ["**/*.test.ts"],
     collectCoverageFrom: [
         "src/**/*.ts",
