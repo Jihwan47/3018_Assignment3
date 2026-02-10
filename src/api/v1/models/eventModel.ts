270324f4a9b302a00d2d2dd1af3be059d492d27c
JayHwang <hjik7297@gmail.com>
2026-02-09 22:41:57 -0600

    feat:modification in interface

---
+++ src/api/v1/models/eventModel.ts
@@ -0,0 +1,44 @@
+export enum Status {
+    active = "active",
+    cancelled = "cancelled",
+    completed = "completed"
+}
+
+export enum Category {
+    conference = "conference",
+    workshop = "workshop",
+    meetup = "meetup",
+    seminar = "seminar",
+    general = "general"
+}
+
+export interface CreateEventRequest{
+    name: string,
+    date: Date,
+    capacity: number,
+    registrationCount?: number,
+    status?: Status,
+    category?: Category
+}
+
+export interface Event {
+    id: string,
+    name: string,
+    date: Date,
+    capacity: number,
+    registrationCount?: number,
+    status?: Status,
+    category?: Category,
+    createdAt: Date,
+    updatedAt: Date
+}
+
+export interface ApiResponse <T>{
+    message: string,
+    data: T
+}
+
+//example be 
+//const response: ApiResponse<Event>
+// message: message
+// data: event
--- src/api/v1/validation/eventValidation.ts
+++ src/api/v1/validation/eventValidation.ts
@@ -1,5 +1,4 @@
 import Joi, { date, ObjectSchema } from "joi";
-import { Category } from "../models/eventModel";
 
 // Post operation schemas organized by request part
 export const postSchemas = {
@@ -7,33 +6,35 @@ export const postSchemas = {
     create: {
         body: Joi.object({
             name: Joi.string().min(3).required().messages({
-                "any.required": "name is required",
-                "string.empty": "name cannot be empty",
-                "string.min": "name length must be at least 3 characters long",
-                "string.base": "name must be a string"
+                "any.required": '"name" is required',
+                "string.empty": '"name" cannot be empty',
+                "string.min": '"name" length must be at least 3 characters long',
+                "string.base": '"name" must be a string'
             }),
 
-            date: Joi.date().greater('now').messages({
-                "date.greater": "date must be greater than now"
+            date: Joi.date().iso().greater('now').default(() => new Date()).messages({
+                "date.greater": '"date" must be greater than now',
+                "date.format": "'date' must be in ISO format"
             }),
 
-            capacity: Joi.number().integer().min(5).integer().messages({
-                "number.min": "capacity must be greater than or equal to 5",
-                "number.integer": "capacity must be an integer",
+            capacity: Joi.number().integer().min(5).default(5).messages({
+                "number.min": '"capacity" must be greater than or equal to 5',
+                "number.integer": '"capacity" must be an integer"',
+                "number.base": '"capacity" must be a number'
             }),
 
-            registrationCount: Joi.number().integer().min(0).max(Joi.ref('capacity')).messages({
-                "number.max": "registrationCount must be less than or equal to ref:capacity",
-                "number.min": "registrationCount must be greater than 0",
-                "number.integer": "registrationCount must be an integer",
+            registrationCount: Joi.number().integer().min(0).default(0).max(Joi.ref('capacity')).messages({
+                "number.max": '"registrationCount" must be less than or equal to ref:capacity',
+                "number.min": '"registrationCount" must be greater than 0',
+                "number.integer": '"registrationCount" must be an integer',
             }),
 
             status: Joi.string().valid("active", "cancelled", "completed").default("active").messages({
-                "string.valid": "status must be one of [active, cancelled, completed]"
+                "string.valid": '"status" must be one of [active, cancelled, completed]'
             }),
 
             category: Joi.string().valid("conference", "workshop", "meetup", "seminar", "general").default("general").messages({
-                "string.valid": "category must be one of [conference, workshop, meetup, seminar, general]"
+                "string.valid": '"category" must be one of [conference, workshop, meetup, seminar, general]'
             }),
         }),
     },
