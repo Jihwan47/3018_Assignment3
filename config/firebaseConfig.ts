edd699373bf380c08eba373dfc74463fa532838d
JayHwang <hjik7297@gmail.com>
2026-02-11 23:46:28 -0600

    feat: adding firebase configuration

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,7 +1,7 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
 
-import * as serviceAccount from "../social-post-db-62b6e-firebase-adminsdk-fbsvc-cd5c21bfb8.json";
+import * as serviceAccount from "../config/social-post-db-62b6e-firebase-adminsdk-fbsvc-cd5c21bfb8.json";
 
 // Initialize the Firebase app with the service account credentials
 // This step is necessary before you can use any Firebase services
