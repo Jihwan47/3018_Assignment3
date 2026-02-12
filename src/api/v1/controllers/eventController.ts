c3c034cbe3774388463e5074d152d2acb36b4983
JayHwang <hjik7297@gmail.com>
2026-02-12 04:08:28 -0600

    feat: added getEventById function

--- src/api/v1/controllers/eventController.ts
+++ src/api/v1/controllers/eventController.ts
@@ -21,34 +21,3 @@ export const getEventById = async (req: Request, res: Response, next: NextFuncti
         next(error);
     }
 };
-
-export const createEvent = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const event = await eventService.createEvent(req.body);
-        res.status(200).json(successResponse(event, "Event created succesfully"));
-    } catch (error: unknown) {
-        next(error);
-    }
-};
-
-export const updateEvent = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const id = String(req.params.id);
-
-        const event = await eventService.updateEvent(id, req.body);
-        res.status(200).json(successResponse(event, "Event updated succesfully"));
-    } catch (error: unknown) {
-        next(error);
-    }
-};
-
-export const deleteEvent = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const id = String(req.params.id);
-
-        const event = await eventService.deleteEvent(id);
-        res.status(200).json(successResponse(event, "Event deleted succesfully"));
-    } catch (error: unknown) {
-        next(error);
-    }
-};
