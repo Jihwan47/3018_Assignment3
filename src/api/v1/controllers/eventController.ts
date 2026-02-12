d55a0bdceaa0ffd8480d63691a731d0519b49cee
JayHwang <hjik7297@gmail.com>
2026-02-12 04:09:07 -0600

    feat: added updateEvent function

--- src/api/v1/controllers/eventController.ts
+++ src/api/v1/controllers/eventController.ts
@@ -31,4 +31,15 @@ export const createEvent = async (req: Request, res: Response, next: NextFunctio
     }
 };
 
+export const updateEvent = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const id = String(req.params.id);
+
+        const event = await eventService.updateEvent(id, req.body);
+        res.status(200).json(successResponse(event, "Event updated succesfully"));
+    } catch (error: unknown) {
+        next(error);
+    }
+};
+
 
