cea20a48064872c12c16494b0b7aac9de1e35ef1
JayHwang <hjik7297@gmail.com>
2026-02-16 01:43:12 -0600

    feat: added comments

--- src/api/v1/models/postModel.ts
+++ src/api/v1/models/postModel.ts
@@ -1,9 +1,20 @@
+
+/**
+ * Represents enumeration of Status of an event
+ * active, cancelled and completed
+ * Default value of active will be inserted, if not provided
+ */
 export enum Status {
     active = "active",
     cancelled = "cancelled",
     completed = "completed"
 }
 
+/**
+ * Represents enumeration of Category of an event
+ * conference, workshop, meetup, seminar and general
+ * Default value of general will be inserted, if not provided
+ */
 export enum Category {
     conference = "conference",
     workshop = "workshop",
@@ -12,6 +23,15 @@ export enum Category {
     general = "general"
 }
 
+/**
+ * Represents request format of event interface
+ * @param name - name of an event
+ * @param date - date of an event
+ * @param capacity - capacity of an event
+ * @param registrationCount - number of registration of an event
+ * @param status - status of an event
+ * @param category - category of an event 
+ */
 export interface CreateEventRequest{
     name: string,
     date: Date,
@@ -21,6 +41,18 @@ export interface CreateEventRequest{
     category?: Category
 }
 
+/**
+ * Represents response format of event interface
+ * @param id - unique identifier of an event
+ * @param name - name of an event
+ * @param date - date of an event
+ * @param capacity - capacity of an event
+ * @param registrationCount - number of registration of an event
+ * @param status - status of an event
+ * @param category - category of an event 
+ * @param createdAt - date when the event was created
+ * @param updatedAt - date when the event was lastly updated
+ */
 export interface Event {
     id: string,
     name: string,
@@ -32,9 +64,3 @@ export interface Event {
     createdAt: Date,
     updatedAt: Date
 }
-
-
-//example be 
-//const response: ApiResponse<Event>
-// message: message
-// data: event
