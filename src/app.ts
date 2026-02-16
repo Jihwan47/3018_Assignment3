855cf61f335db5ce7e0c86efd51f24df7a4f3ffa
JayHwang <hjik7297@gmail.com>
2026-02-15 21:23:57 -0600

    feat: added route handler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import express, { Express } from "express";
 
+import healthRoutes from "./api/v1/routes/healthRoutes"
+import eventRoutes from "./api/v1/routes/eventRoutes"
+
 // Initialize Express application
 const app: Express = express();
+app.use(express.json());
 
 // Define a route
 app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
+
+// router handler
+// router defined in health routes, prefixed with /api/v1
+app.use("/api/v1", healthRoutes);
+app.use("/api/v1", eventRoutes);
 
 export default app;
