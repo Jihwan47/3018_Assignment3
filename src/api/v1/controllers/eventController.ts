9020d852c6dab4321119b090c6e24ae16fb78a04
JayHwang <hjik7297@gmail.com>
2026-02-16 01:20:09 -0600

    feat: added comments

--- src/api/v1/controllers/eventController.ts
+++ src/api/v1/controllers/eventController.ts
@@ -2,9 +2,16 @@ import { NextFunction, Request, Response } from "express";
 import { successResponse } from "../models/responseModel";
 import * as eventService from "../services/eventService"
 
+/**
+ * GET /api/v1/events
+ * Retrieve all events in the database
+ * validate required fields
+ */
 export const getAllEvent = async (req: Request, res: Response, next: NextFunction) => {
     try {
+        // get all events
         const event = await eventService.getAllEvents();
+        // count the number of events in the database
         const totalCount = event.length;
         res.status(200).json(successResponse(event, "Succesfully retreived", totalCount));
     } catch (error: unknown) {
@@ -12,6 +19,10 @@ export const getAllEvent = async (req: Request, res: Response, next: NextFunctio
     }
 };
 
+/**
+ * GET /api/v1/events/:id
+ * retreive a specific events by its id
+ */
 export const getEventById = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const id = String(req.params.id);
@@ -23,6 +34,10 @@ export const getEventById = async (req: Request, res: Response, next: NextFuncti
     }
 };
 
+/**
+ * POST /api/v1/events
+ * create a new event
+ */
 export const createEvent = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const event = await eventService.createEvent(req.body);
@@ -32,6 +47,10 @@ export const createEvent = async (req: Request, res: Response, next: NextFunctio
     }
 };
 
+/**
+ * PUT /api/v1/events/:id
+ * update a specific event by its id
+ */
 export const updateEvent = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const id = String(req.params.id);
@@ -43,6 +62,10 @@ export const updateEvent = async (req: Request, res: Response, next: NextFunctio
     }
 };
 
+/**
+ * DELETE /api/v1/events/:id
+ * delete a specific event by its id
+ */
 export const deleteEvent = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const id = String(req.params.id);
