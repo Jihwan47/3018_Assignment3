6771b80f4814a696e47658ce6c438532e257d9fb
JayHwang <hjik7297@gmail.com>
2026-02-19 03:00:15 -0600

    feat: no firebase serviceAccount allowed in github..

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,16 +1,32 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
-import { getFirestore, Firestore } from "firebase-admin/firestore";
+import { getFirestore } from "firebase-admin/firestore";
 
-import * as serviceAccount from "../config/social-post-db-62b6e-firebase-adminsdk-fbsvc-cd5c21bfb8.json";
+const serviceAccount: ServiceAccount = {
+  projectId: "dummy",
+  clientEmail: "dummy@example.com",
+  privateKey: "dummy",
+};
 
-// Initialize the Firebase app with the service account credentials
-// This step is necessary before you can use any Firebase services
-initializeApp({
-    credential: cert(serviceAccount as ServiceAccount),
-});
-
-// Get a reference to the Firestore service
-// This creates a Firestore instance that you can use to interact with your database
-const db: Firestore = getFirestore();
-
+initializeApp({ credential: cert(serviceAccount) });
+const db = getFirestore();
 export { db };
+
+
+
+
+// import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
+// import { getFirestore, Firestore } from "firebase-admin/firestore";
+
+// import * as serviceAccount from "../config/social-post-db-62b6e-firebase-adminsdk-fbsvc-cd5c21bfb8.json";
+
+// // Initialize the Firebase app with the service account credentials
+// // This step is necessary before you can use any Firebase services
+// initializeApp({
+//     credential: cert(serviceAccount as ServiceAccount),
+// });
+
+// // Get a reference to the Firestore service
+// // This creates a Firestore instance that you can use to interact with your database
+// const db: Firestore = getFirestore();
+
+// export { db };
