40c3bec7541f5d179255336ff7389868c8a662a0
JayHwang <hjik7297@gmail.com>
2026-03-26 22:49:45 -0500

    feat: added dnsPrefetchControl in helmet.ts

--- src/api/v1/middleware/helmet.ts
+++ src/api/v1/middleware/helmet.ts
@@ -10,7 +10,7 @@ const getHelmetConfig = () => {
             contentSecurityPolicy: false, // Disable CSP in development
             hsts: false, // No HTTPS enforcement in development
             hidePoweredBy: true, // hide freamwork, version or any vulnurability information
-            // dnsPrefetchControl: false, // Most of the browser prefetch DNS to improve performance, but it can be a privacy concern. Privacy Trade-off
+            dnsPrefetchControl: false, // Most of the browser prefetch DNS to improve performance, but it can be a privacy concern. Privacy Trade-off
             // frameguard: {action: "deny"}, // Prevent clickjacking by disallowing the application from being embedded in frames.
             // noSniff: true, // Prevent MIME type sniffing, which can lead to security vulnerabilities.
         });
@@ -33,14 +33,11 @@ const getHelmetConfig = () => {
         // // Prevent MIME type sniffing
         // noSniff: true,
 
-        // // Set referrer policy for API responses
-        // referrerPolicy: { policy: "no-referrer" },
-
         // // Prevent clickjacking by denying framing
         // frameguard: { action: "deny" },
 
-        // // Disable DNS prefetching for privacy
-        // dnsPrefetchControl: { allow: false },
+        // Disable DNS prefetching for privacy
+        dnsPrefetchControl: { allow: false },
         
     });
 };
