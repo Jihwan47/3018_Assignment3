04575bea290c84fd5a6a35e46c2ffb5038d4fa01
JayHwang <hjik7297@gmail.com>
2026-03-26 22:50:59 -0500

    feat: added helmet security in App.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,31 @@
 import express, { Express } from "express";
-import morgan from "morgan";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+import morgan from "morgan";
+import getHelmetConfig from "./api/v1/middleware/helmet";
+//import cors from "cors";
 import healthRoutes from "./api/v1/routes/healthRoutes"
 import eventRoutes from "./api/v1/routes/eventRoutes"
+//import getCorsOptions from "./api/v1/middleware/cors";
+
+// import setupSwagger from "../config/swagger";
+// import itemRoutes from "./api/v1/routes/itemRoutes";
+// import userRoutes from "./api/v1/routes/userRoutes";
+// import adminRoutes from "./api/v1/routes/adminRoutes";
+// import errorHandler from "./api/v1/middleware/errorHandler";
+
 
 // Initialize Express application
 const app: Express = express();
+
+// Enable CORS for all routes
+//app.use(cors(getCorsOptions()));
+
+// helmet for security
+app.use(getHelmetConfig());
+
 app.use(express.json());
 
 // Use Morgan for HTTP request logging
@@ -15,6 +35,7 @@ app.use(morgan("combined"));
 app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
+
 
 // router handler
 // router defined in health routes, prefixed with /api/v1
