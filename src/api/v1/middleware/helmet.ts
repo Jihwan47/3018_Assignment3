225bd1143423b9b1b5bf88754edbbca63f15d7a5
JayHwang <hjik7297@gmail.com>
2026-03-26 22:50:08 -0500

    feat: added frameguard in helmet.ts

--- src/api/v1/middleware/helmet.ts
+++ src/api/v1/middleware/helmet.ts
@@ -11,7 +11,7 @@ const getHelmetConfig = () => {
             hsts: false, // No HTTPS enforcement in development
             hidePoweredBy: true, // hide freamwork, version or any vulnurability information
             dnsPrefetchControl: false, // Most of the browser prefetch DNS to improve performance, but it can be a privacy concern. Privacy Trade-off
-            // frameguard: {action: "deny"}, // Prevent clickjacking by disallowing the application from being embedded in frames.
+            frameguard: {action: "deny"}, // Prevent clickjacking by disallowing the application from being embedded in frames.
             // noSniff: true, // Prevent MIME type sniffing, which can lead to security vulnerabilities.
         });
     }
@@ -33,8 +33,8 @@ const getHelmetConfig = () => {
         // // Prevent MIME type sniffing
         // noSniff: true,
 
-        // // Prevent clickjacking by denying framing
-        // frameguard: { action: "deny" },
+        // Prevent clickjacking by denying framing
+        frameguard: { action: "deny" },
 
         // Disable DNS prefetching for privacy
         dnsPrefetchControl: { allow: false },
